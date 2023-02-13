e5b3d8d19ccb7ba6c58c2097369aa1453b0d4f98
muh ali bakhtiar <muhalibakhtiar@gmail.com>
2023-02-14 05:09:24 +0700

    add materi python basic

---
+++ python_basic/comment_var.py
@@ -0,0 +1,4 @@
+#belajar variabel
+
+x = "Hello World"
+print(x)
---
+++ python_basic/tipe_data.py
@@ -0,0 +1,40 @@
+x = "1"
+z = 1
+
+print(type(x), type(z))
+print(x, z)
+print(float(z))
+
+
+print('data type : list')
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(f'list data : {fruits}')
+
+count = fruits.count('apple')
+print(f'list data with count : {count}')
+
+count_tangerine = fruits.count('tangerine')
+print(f'list data with count_tangerine : {count_tangerine}')
+
+index = fruits.index('banana')
+print(f'list data with index : {index}')
+
+index_position = fruits.index('banana', 4)  # Find next banana starting a position 4
+print(f'list data with index_position : {index_position}')
+
+reverse = fruits.reverse()
+print(f'list data with reverse : {reverse}')
+
+_append = fruits.append('grape')
+print(f'list data with append : {_append}')
+
+sort = fruits.sort()
+
+print(f'list data with sort : {fruits}')
+
+pop = fruits.pop()
+print(f'list data with pop : {pop}')
+
+clear = fruits.clear()
+print(f'list data with clear : {clear}')
