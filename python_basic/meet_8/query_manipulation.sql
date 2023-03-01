111a1360043123db79dcf5f8c783e35471bd9226
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2023-03-01 16:32:42 +0700

    udpate materi pertemuan 8| query

--- python_basic/meet_8/query_manipulation.sql
+++ python_basic/meet_8/query_manipulation.sql
@@ -11,3 +11,24 @@ INSERT INTO customers (NAMAID, NAMALENGKAP, EMAIL)
 values ("Astrid", "Astrid Gruber", "astrid.gruber@apple.at")
 ;
 
+-- insert banyak
+INSERT INTO customers (NAMAID, NAMALENGKAP, EMAIL) 
+values 
+("Fernanda","Fernanda Ramos","fernadaramos4@uol.com.br"),
+("Mark","Mark Philips","mphilips12@shaw.ca"),
+("Jennifer","Jennifer Peterson","jenniferp@rogers.ca")
+;
+
+-- mengubah data 
+UPDATE customers 
+SET 
+    NAMALENGKAP = "Jennifer Lauren",
+    EMAIL = "jenniferl@rogers.ca"
+WHERE
+    ID = 4
+;
+
+-- menghapus data 
+DELETE FROM customers
+WHERE ID = 4
+;
